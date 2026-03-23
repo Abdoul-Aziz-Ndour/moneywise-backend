6cf7863c2b30542dc0f88b1610d3c2df693cafa6
fatmata12 <fc5434653@gmail.com>
2026-03-23 09:50:02 +0100

    feat: models, views et admin - Fatoumata

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User, Categorie, Transaction, AlerteBudget
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ['email', 'username', 'date_creation', 'is_staff']
+
+@admin.register(Categorie)
+class CategorieAdmin(admin.ModelAdmin):
+    list_display = ['nom', 'type', 'utilisateur']
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ['utilisateur', 'type', 'montant', 'categorie', 'date']
+
+@admin.register(AlerteBudget)
+class AlerteBudgetAdmin(admin.ModelAdmin):
+    list_display = ['utilisateur', 'categorie', 'seuil', 'actif']
